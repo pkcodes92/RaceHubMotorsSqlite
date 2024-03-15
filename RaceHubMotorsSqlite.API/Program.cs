b9ca5dfbc90f588fc080930af047be1bef61dcae
Pranav Krishnamurthy <pkrish19@outlook.com>
2024-03-15 16:22:20 -0400

    MOT-1 Ensuring that the database context is properly injected

--- RaceHubMotorsSqlite.API/Program.cs
+++ RaceHubMotorsSqlite.API/Program.cs
@@ -1,9 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using RaceHubMotorsSqlite.API.DAL.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+var connectionString = builder.Configuration.GetConnectionString("FileLocation");
+builder.Services.AddDbContext<MotorsContext>(options => {
+    options.UseSqlite(connectionString);
+});
 
 var app = builder.Build();
 
