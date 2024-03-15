cb829337e2e6c20a6745cb9d0f228ac07b451320
Pranav Krishnamurthy <pkrish19@outlook.com>
2024-03-15 16:15:20 -0400

    MOT-1 Ensuring that all of the code is at the latest

--- RaceHubMotorsSqlite.API.DAL/Context/MotorsContext.cs
+++ RaceHubMotorsSqlite.API.DAL/Context/MotorsContext.cs
@@ -1,8 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace RaceHubMotorsSqlite.API.DAL.Context;
 
-public partial class MotorsContext(DbContextOptions<MotorsContext> options): DbContext(options)
+public partial class MotorsContext(DbContextOptions<MotorsContext> options) : DbContext(options)
 {
-    protected override void OnModelCreating(ModelBuilder modelBuilder) 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
 
     }
