29ace6ba5f1853f6f94d9ad55d69068c03ae03a8
Pranav Krishnamurthy <pkrish19@outlook.com>
2024-03-16 11:55:00 -0400

    MOT-2 Ensuring to have the right properties created

--- RaceHubMotorsSqlite.API.DAL/Models/Manufacturer.cs
+++ RaceHubMotorsSqlite.API.DAL/Models/Manufacturer.cs
@@ -1,6 +1,18 @@
+#nullable disable
+
 namespace RaceHubMotorsSqlite.API.DAL.Models;
 
 public class Manufacturer
 {
     public int ManufacturerId { get; set; }
+
+    public string Name { get; set; }
+
+    public string Country { get; set; }
+
+    public string Logo { get; set; }
+
+    public string NationalFlag { get; set; }
+
+    public string Region { get; set; }
 }
