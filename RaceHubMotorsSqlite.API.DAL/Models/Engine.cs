47258eeb6fa7752d783d816a894131172bcb75d2
Pranav Krishnamurthy <pkrish19@outlook.com>
2024-03-18 20:21:22 -0400

    MOT-2 Adding the engine entity

---
+++ RaceHubMotorsSqlite.API.DAL/Models/Engine.cs
@@ -0,0 +1,12 @@
+#nullable disable
+
+namespace RaceHubMotorsSqlite.API.DAL.Models;
+
+public class Engine
+{
+    public int EngineId { get; set; }
+
+    public string Code { get; set; }
+
+    public string Description { get; set; }
+}
