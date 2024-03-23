24d16358d353cfd3e182a9885b7b7bed8cfd8142
Pranav Krishnamurthy <pkrish19@outlook.com>
2024-03-23 12:37:45 -0400

    MOT-2 Ensuring to have all of the engine related changes done

---
+++ RaceHubMotorsSqlite.API.DAL/Repository/EngineRepository.cs
@@ -0,0 +1,21 @@
+﻿using RaceHubMotorsSqlite.API.DAL.Models;
+
+namespace RaceHubMotorsSqlite.API.DAL;
+
+public class EngineRepository : IEngineRepository
+{
+    public Task<List<Engine>> GetAllEnginesAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Engine> GetEngineByCodeAsync(string code)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Engine> GetEngineByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
