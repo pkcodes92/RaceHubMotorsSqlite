89b799ffd421dfc032953d0ee1a180da46eb0a8c
Pranav Krishnamurthy <pkrish19@outlook.com>
2024-07-14 14:47:35 -0400

    Making sure that all of the database configurations are in the right class and defined properly

---
+++ RaceHubMotorsSqlite.API.Common/Configurations/DatabaseSettings.cs
@@ -0,0 +1,14 @@
+﻿#nullable disable
+
+namespace RaceHubMotorsSqlite.API.Common.Configurations;
+
+/// <summary>
+/// This class will define the necessary database configurations.
+/// </summary>
+public class DatabaseSettings
+{
+    /// <summary>
+    /// Gets or sets the file location.
+    /// </summary>
+    public string FileLocation { get; set; }
+}
