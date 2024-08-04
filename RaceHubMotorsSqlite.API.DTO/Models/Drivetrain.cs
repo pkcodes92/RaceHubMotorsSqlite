6735b873e68152c2f6d53bae7cf7bec79a62a608
Pranav Krishnamurthy <pkrish19@outlook.com>
2024-08-04 12:22:20 -0400

    MOT-3 Making sure that all of the code changes are being done

--- RaceHubMotorsSqlite.API.DTO/Models/Drivetrain.cs
+++ RaceHubMotorsSqlite.API.DTO/Models/Drivetrain.cs
@@ -1,29 +1,34 @@
 ﻿#nullable disable
 
 using Newtonsoft.Json;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace RaceHubMotorsSqlite.API.DTO.Models;
 
 /// <summary>
 /// This class defines the Drivetrain UI entity.
 /// </summary>
+[SwaggerSchema(Description = "This is the definition of a drivetrain")]
 public class Drivetrain
 {
     /// <summary>
     /// Gets or sets the primary key - the ID.
     /// </summary>
     [JsonProperty("id")]
+    [SwaggerSchema(Description = "The primary key of the Drivetrain UI entity - ID")]
     public int Id { get; set; }
 
     /// <summary>
     /// Gets or sets the code.
     /// </summary>
     [JsonProperty("code")]
+    [SwaggerSchema(Description = "The code of the drivetrain UI entity")]
     public string Code { get; set; }
 
     /// <summary>
     /// Gets or sets the description.
     /// </summary>
     [JsonProperty("description")]
+    [SwaggerSchema(Description = "The description of the drivetrain UI entity")]
     public string Description { get; set; }
 }
--- RaceHubMotorsSqlite.API.DTO/Models/VehicleColor.cs
+++ RaceHubMotorsSqlite.API.DTO/Models/VehicleColor.cs
@@ -1,29 +1,34 @@
 #nullable disable
 
 using Newtonsoft.Json;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace RaceHubMotorsSqlite.API.DTO.Models;
 
 /// <summary>
 /// This class represents the Vehicle Color UI entity.
 /// </summary>
+[SwaggerSchema(Description = "The vehicle color UI entity")]
 public class VehicleColor
 {
     /// <summary>
     /// Gets or sets the primary key - the ID.
     /// </summary>
     [JsonProperty("id")]
+    [SwaggerSchema(Description = "The primary key - the ID")]
     public int Id { get; set; }
 
     /// <summary>
     /// Gets or sets the code.
     /// </summary>
     [JsonProperty("code")]
+    [SwaggerSchema(Description = "The code of the vehicle color")]
     public string Code { get; set; }
 
     /// <summary>
     /// Gets or sets the description.
     /// </summary>
     [JsonProperty("description")]
+    [SwaggerSchema(Description = "The description of the vehicle color")]
     public string Description { get; set; }
 }
