d2c557bee416776c2790155b7636470779c88b26
Pranav Krishnamurthy <pkrish19@outlook.com>
2024-08-04 15:52:21 -0400

    MOT-3 Making sure to have all of the XML documentation

--- RaceHubMotorsSqlite.API.DTO/Request/AddEngineRequest.cs
+++ RaceHubMotorsSqlite.API.DTO/Request/AddEngineRequest.cs
@@ -1,8 +1,27 @@
+#nullable disable
+
+using Newtonsoft.Json;
+using Swashbuckle.AspNetCore.Annotations;
+
 namespace RaceHubMotorsSqlite.API.DTO.Request;
 
+/// <summary>
+/// This class represents the request to add a new engine.
+/// </summary>
+[SwaggerSchema(Description = "The request for adding a new engine")]
 public class AddEngineRequest
 {
+    /// <summary>
+    /// This is the code of the new engine being added.
+    /// </summary>
+    [JsonProperty("code")]
+    [SwaggerSchema(Description = "The code of the new engine being added")]
     public string Code { get; set; }
 
+    /// <summary>
+    /// This is the description of the new engine being added.
+    /// </summary>
+    [JsonProperty("description")]
+    [SwaggerSchema(Description = "The description of the new engine being added")]
     public string Description { get; set; }
 }
--- RaceHubMotorsSqlite.API.DTO/Request/UpdateEngineRequest.cs
+++ RaceHubMotorsSqlite.API.DTO/Request/UpdateEngineRequest.cs
@@ -1,10 +1,33 @@
+#nullable disable
+
+using Newtonsoft.Json;
+using Swashbuckle.AspNetCore.Annotations;
+
 namespace RaceHubMotorsSqlite.API.DTO.Request;
 
+/// <summary>
+/// This is the request for an existing engine to be updated.
+/// </summary>
 public class UpdateEngineRequest
 {
+    /// <summary>
+    /// This is the primary key of the engine being updated.
+    /// </summary>
+    [JsonProperty("id")]
+    [SwaggerSchema(Description = "The primary key of the Engine ID to be updated")]
     public int Id { get; set; }
 
+    /// <summary>
+    /// The code of the engine being updated.
+    /// </summary>
+    [JsonProperty("code")]
+    [SwaggerSchema(Description = "The code of the engine being updated")]
     public string Code { get; set; }
 
+    /// <summary>
+    /// The description of the engine being updated.
+    /// </summary>
+    [JsonProperty("description")]
+    [SwaggerSchema(Description = "The description of the updated engine information")]
     public string Description { get; set; }
 }
