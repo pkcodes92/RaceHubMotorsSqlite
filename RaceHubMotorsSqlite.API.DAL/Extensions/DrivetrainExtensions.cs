6437f4a9775fe51098a199b702c7d573a14b7af1
Pranav Krishnamurthy <pkrish19@outlook.com>
2025-03-29 13:23:42 -0400

    Ensuring that all of the various things are being done in the correct way with the XML documentation

--- RaceHubMotorsSqlite.API.DAL/Extensions/DrivetrainExtensions.cs
+++ RaceHubMotorsSqlite.API.DAL/Extensions/DrivetrainExtensions.cs
@@ -2,8 +2,17 @@ using RaceHubMotorsSqlite.API.DAL.Models;
 
 namespace RaceHubMotorsSqlite.API.DAL.Extensions;
 
+/// <summary>
+/// This class extends the type of <see cref="Drivetrain"/>.
+/// </summary>
+/// <remarks>Initializes an instance of <see cref="DrivetrainExtensions"/>.</remarks>
 public static class DrivetrainExtensions 
 {
+    /// <summary>
+    /// This method converts a database entity to a DTO entity.
+    /// </summary>
+    /// <param name="value">The drivetrain information from the database.</param>
+    /// <returns>A type of <see cref="DTO.Models.Drivetrain"/>.</returns>
     public static DTO.Models.Drivetrain ToDto(this Drivetrain value)
     {
         return new DTO.Models.Drivetrain
